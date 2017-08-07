4bb07563ff690e5a3e987e298c9d57988f8499bc
vvarun77 <vvijayababu4334@gmail.com>
2017-08-06 21:26:57 -0700

    Add files via upload

--- Application.java
+++ Application.java
@@ -4,22 +4,15 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		System.out.print("grade");
+		System.out.print("numberoflaps");
 		Scanner scanner = new Scanner(System.in);
-		int grade = scanner.nextInt();
+		int numberoflaps = scanner.nextInt();
 
-		if (grade <= 5) {
-			System.out.println("elementary");
+		System.out.print("runtime");
 
-		} else if (grade >= 5 && grade <= 9) {
-			System.out.println("middle");
-			
-		} else if (grade >= 8 && grade <= 13) {
-			System.out.println("high");
-			
-		} else {
-			System.out.println("college");
-		}
+		int runtime = scanner.nextInt();
+
+		System.out.println(runtime * numberoflaps);
 	}
 
 }
